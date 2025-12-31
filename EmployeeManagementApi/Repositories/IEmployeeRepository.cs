f6f79ca354dd40fdebcda5e3456f334531573125
Adam <adamchouhani19@gmail.com>
2026-01-01 00:06:41 +0100

    creating the repository interface

---
+++ EmployeeManagementApi/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementApi.Models;
+
+namespace EmployeeManagementApi.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee> GetByIdAsync();
+        Task AddEmployeeAsync(Employee employee);
+        Task UpdateEmployeeAsync(Employee employee);
+        Task DeleteEmployeeAsync(Employee employee);
+    }
+}
