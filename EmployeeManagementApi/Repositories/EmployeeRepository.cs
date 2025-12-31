ee91953843212e81bf0347a839d6adf38df46367
Adam <adamchouhani19@gmail.com>
2026-01-01 00:26:22 +0100

    repository logic for adding

---
+++ EmployeeManagementApi/Repositories/EmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagementApi.Data;
+using EmployeeManagementApi.Models;
+
+namespace EmployeeManagementApi.Repositories
+{
+    public class EmployeeRepository(AppDbContext context) : IEmployeeRepository
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteEmployeeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
