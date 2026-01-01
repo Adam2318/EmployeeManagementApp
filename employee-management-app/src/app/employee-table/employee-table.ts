dd9745adba488b294a5744a1bc7e9893d8e30f1f
Adam <adamchouhani19@gmail.com>
2026-01-01 12:35:45 +0100

    showing the employees data in a table

--- employee-management-app/src/app/employee-table/employee-table.ts
+++ employee-management-app/src/app/employee-table/employee-table.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'employee-table',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './employee-table.css',
 })
 export class EmployeeTable {
+
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(){
+    this.employeeService.getEmployees().subscribe((data: Employee[]) => {
+      this.employees = data;
+      console.log(data);
+    });
+  }
 
 }
