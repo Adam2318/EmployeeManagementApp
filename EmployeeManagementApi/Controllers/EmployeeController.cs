fd9136f3f8a10424475e3e9f76c1b733b75cda20
Adam <adamchouhani19@gmail.com>
2026-01-01 10:17:28 +0100

    Implementing CRUD endpoints

--- EmployeeManagementApi/Controllers/EmployeeController.cs
+++ EmployeeManagementApi/Controllers/EmployeeController.cs
@@ -10,12 +10,48 @@ namespace EmployeeManagementApi.Controllers
     {
         private readonly IEmployeeRepository _employeeRepository = employeeRepository;
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployeesAsync()
+        {
+            var allEmployees = await _employeeRepository.GetAllAsync();
+            return Ok(allEmployees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
         [HttpPost]
         public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
         {
             await _employeeRepository.AddEmployeeAsync(employee);
-            return Created();
+            return CreatedAtAction(nameof(GetEmployeeById), new {id = employee.Id}, employee);
         }
-                
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeAsync(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            await _employeeRepository.UpdateEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployeeById(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
     }
 }
